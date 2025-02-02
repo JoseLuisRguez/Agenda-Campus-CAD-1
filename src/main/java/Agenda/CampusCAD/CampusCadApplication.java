fe2695e4e3f5907d6ddb8e257982d770357c775d
JoseLuisRguez <150103836+JoseLuisRguez@users.noreply.github.com>
2025-02-02 03:16:21 -0600

    Update CampusCadApplication.java

--- src/main/java/Agenda/CampusCAD/CampusCadApplication.java
+++ src/main/java/Agenda/CampusCAD/CampusCadApplication.java
@@ -19,7 +19,8 @@ public class CampusCadApplication {
             @Override
             public void addCorsMappings(CorsRegistry registry){
             registry.addMapping("/**")
-                    .allowedOrigins("https://agenda-campus-852bb.web.app", "http://localhost:8100") // Especifica el dominio
+		    .allowedOrigins("http://localhost:8100")
+                    .allowedOrigins("https://agenda-campus-852bb.web.app") // Especifica el dominio
    	            .allowCredentials(true) // Permite credenciales como cookies o tokens
                     .allowedHeaders("*")
                     .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH");
