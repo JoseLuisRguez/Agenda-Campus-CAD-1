cd44fcaf76e742906ffee9404c0069ea3fca9b7e
JoseLuisRguez <150103836+JoseLuisRguez@users.noreply.github.com>
2025-03-28 14:32:05 -0600

    Delete src directory

--- src/main/java/Agenda/CampusCAD/Repositorios/CanchaRepository.java
+++
@@ -1,8 +0,0 @@
-package Agenda.CampusCAD.Repositorios;
-
-import Agenda.CampusCAD.Entidades.Cancha;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CanchaRepository extends JpaRepository<Cancha, Long> {
-    // Puedes agregar métodos adicionales de consulta si es necesario
-}
